7b7af238ca409dd1300d42263eebbac99081f62f
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-04-06 15:49:21 +0300

    Method added draw_grid

--- bin/main.rb
+++ bin/main.rb
@@ -21,4 +21,40 @@ def show_board(board)
   draw_grid(board_show)
 end
 
-puts "Hello!"
+def draw_grid(array)
+  hypens = '-------------------'
+  hypens_w_nl = "-------------------\n"
+  puts "\n  " + hypens
+  3.times do |i|
+    print '  | ', array[i * 3], ' | ', array[i * 3 + 1], ' | ', array[i * 3 + 2], " |\n  #{hypens_w_nl if i < 2}"
+  end
+  puts hypens
+end
+
+# def slow_typing(text, speed)
+#   text.size.times do |i|
+#     print text[i]
+#     sleep speed * 0.01
+#   end
+#   puts
+# end
+# 
+# def question(turn)
+#   turn = turn.even? ? 'X' : 'O'
+#   text = "Make your move #{turn} (1..9):"
+#   slow_typing(text, 1)
+#   loop do
+#     answer = gets.chomp!.to_i
+#     return answer if (1..9).include? answer
+# 
+#     puts 'Please enter a number between 1 and 9...'
+#   end
+# end
+# 
+# greeting
+# 
+# board = create_board
+# 
+# show_board(board)
+# 
+# question(0)
