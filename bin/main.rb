91af6dfcd842be9c6eb2b94ef2d90c2422fed39c
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-04-06 15:47:30 +0300

    Method created create_board

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
+puts 'Hello!'
+
+def greeting
+  puts "Welcome to TicTacToe game!"
+end
+
+def create_board
+  Array.new(9, 0)
+end
+
+BOARD_CYPHER = { "0": ' . ', "1": ' X ', '10': ' O ' }.freeze
+
 
 puts "Hello!"
