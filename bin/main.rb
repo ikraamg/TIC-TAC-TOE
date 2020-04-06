40016c5fc681e3d1492390e08e3717c64b54540d
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-04-06 15:48:18 +0300

    Method added show_board

--- bin/main.rb
+++ bin/main.rb
@@ -13,5 +13,12 @@ end
 
 BOARD_CYPHER = { "0": ' . ', "1": ' X ', '10': ' O ' }.freeze
 
+def show_board(board)
+  board_show = board.clone
+  board_show.map! do |item|
+    BOARD_CYPHER[item.to_s.to_sym]
+  end
+  draw_grid(board_show)
+end
 
 puts "Hello!"
