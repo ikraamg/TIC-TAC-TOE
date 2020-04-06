4e24651f2b4f8f0403e5bb183354c72af1067c0a
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-04-06 17:20:11 +0300

    Method groups added calculation

--- bin/main.rb
+++ bin/main.rb
@@ -120,5 +120,34 @@ class TicTacToe < Game
     end
   end
 
- 
+  def calculate_board
+    sum_of_lines = []
+    sum_of_lines = calculate_linear(sum_of_lines)
+    calculte_diagonal(sum_of_lines)
+  end
+
+  def calculate_linear(array)
+    3.times do |i|
+      sum_line = @board[i * 3] + @board[i * 3 + 1] + @board[i * 3 + 2]
+      array << sum_line
+      sum_line = @board[i] + @board[i + 3] + @board[i + 6]
+      array << sum_line
+    end
+    array
+  end
+
+  def calculte_diagonal(array)
+    2.times do |i|
+      sum_diagonal = @board[i + i] + @board[4] + @board[8 - i * 2]
+      array << sum_diagonal
+    end
+    array
+  end
+
+  def make_move(move, turn)
+    mark = turn.even? ? 1 : 10
+    @board[move - 1] = mark
+  end
+
+  
 end
