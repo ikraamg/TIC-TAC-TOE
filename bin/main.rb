47f8434fb19dac2c7471cb51465eee302b02037a
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-04-06 17:09:52 +0300

    Module added QuestinMaker

--- bin/main.rb
+++ bin/main.rb
@@ -40,24 +40,26 @@ module Visualizer
   end
 end
 
+module QuestionMaker
+  def question(turn)
+    turn = turn.even? ? 'X' : 'O'
+    text = "Make your move #{turn} (1..9):"
+    slow_typing(text, 1)
+    loop do
+      answer = gets.chomp!.to_i
+      return answer if (1..9).include? answer
+
+      puts 'Please enter a number between 1 and 9...'
+    end
+  end
+end
+
 def greeting
   puts 'Welcome to TicTacToe game!'
 end
 
 def create_board
   Array.new(9, 0)
-end
-
-def question(turn)
-  turn = turn.even? ? 'X' : 'O'
-  text = "Now it is your turn #{turn}. Make your move! (1..9):"
-  slow_typing(text, 1)
-  loop do
-    answer = gets.chomp!.to_i
-    return answer if (1..9).include? answer
-
-    puts 'Please enter a number between 1 and 9...'
-  end
 end
 
 def play
