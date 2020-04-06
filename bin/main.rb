dee9d6ff4b7256853ec9b20c4c6f70a3de3fc147
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-04-06 17:09:17 +0300

    Module added Visualizer

---
+++ bin/main.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+
+module Visualizer
+  def slow_typing(text, speed)
+    text.size.times do |i|
+      print text[i]
+      sleep speed * 0.01
+    end
+    puts
+  end
+  BOARD_CYPHER = { "0": ' . ', "1": ' X ', '10': ' O ' }.freeze
+  def show_board(board)
+    board_show = board.clone
+    board_show.map! do |item|
+      BOARD_CYPHER[item.to_s.to_sym]
+    end
+    draw_grid(board_show)
+  end
+
+  def show_board_hint(board)
+    hint_board = board.clone
+    hint_board.map! do |item|
+      if item.zero?
+        "(#{hint_board.index(item) + 1})"
+      else
+        BOARD_CYPHER[item.to_s.to_sym]
+      end
+    end
+    draw_grid(hint_board)
+  end
+
+  def draw_grid(array)
+    hypens = '-------------------'
+    hypens_w_nl = "-------------------\n"
+    puts "\n  " + hypens
+    3.times do |i|
+      print '  | ', array[i * 3], ' | ', array[i * 3 + 1], ' | ', array[i * 3 + 2], " |\n  #{hypens_w_nl if i < 2}"
+    end
+    puts hypens
+  end
+end
+
+def greeting
+  puts 'Welcome to TicTacToe game!'
+end
+
+def create_board
+  Array.new(9, 0)
+end
+
+def question(turn)
+  turn = turn.even? ? 'X' : 'O'
+  text = "Now it is your turn #{turn}. Make your move! (1..9):"
+  slow_typing(text, 1)
+  loop do
+    answer = gets.chomp!.to_i
+    return answer if (1..9).include? answer
+
+    puts 'Please enter a number between 1 and 9...'
+  end
+end
+
+def play
+  greeting
+
+  board = create_board
+
+  show_board(board)
+
+  9.times do |turn|
+    print "\nThis is turn: #{turn + 1} out of 9\n "
+    question(turn)
+    puts '(Your move is displayed on the board below)'
+    show_board(board)
+  end
+end
+
+play
---
+++ bin/test.rb
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3]
+
+print numbers
+
+numbers_copy = numbers.map { |i| i }
+
+numbers_copy << 4
+
+print numbers
+puts
+print numbers_copy
