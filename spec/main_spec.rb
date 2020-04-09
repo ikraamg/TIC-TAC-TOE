fa1c5a26c0d372bfb9ba70bc04231b1d395cae9b
Ikraam <ikraamghoor@gmail.com>
2020-04-09 12:42:54 +0200

    Tested for TicTacToe until to_s method

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -24,14 +24,42 @@ describe Game do
   end
 end
 
+describe TicTacToe do
+  start_game = TicTacToe.new('Ikraam', 'Kubilay')
+  describe '#initialize' do
+    it 'Reurns an ' do
+      expect { TicTacToe.new('Ikraam', 'Kubilay') }.not_to raise_error(ArgumentError)
+    end
+  end
+  describe '#names' do
+    it 'Reurns an array with two inputs and 5 times' do
+      expect(start_game.names).to eq(%w[Ikraam Kubilay] * 5)
+    end
+  end
+  describe '#board' do
+    it 'returns an array with 9 integers of 0' do
+      expect(start_game.board).to eq(Array.new(9, 0))
+    end
+  end
+  describe '#greeting' do
+    it 'returns the same as start_game.board passed to show_board_hint' do
+      expect(start_game.greeting).to eq(show_board_hint(start_game.board))
+    end
+  end
+  describe '#to_s' do
+    it 'returns correct string' do
+      expect(start_game.to_s).to eq("Welcome\nThis is a TIC TAC TOE game!")
+    end
+  end
+  describe '#check-spot' do
+    it 'returns a string when the spot is already occupied' do
+      expect start_game.check_spot(1, 1).to eq('jump_next')
     end
   end
 end
 
-
-
-tictactoe = TicTacToe.new("Ikraam", "Kubilay")
+tictactoe = TicTacToe.new('Ikraam', 'Kubilay')
 print tictactoe.methods.sort - Object.methods
 puts
-player = Player.new("Kubilay")
+player = Player.new('Kubilay')
 print player.methods.sort - Object.methods
