45dfdf18dff8c744eabc2b60e37cab785a7a6d12
Ikraam <ikraamghoor@gmail.com>
2020-04-09 12:26:18 +0200

    Test for Game methods complete

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,14 +1,28 @@
 require_relative '../bin/main.rb'
 
 describe Game do
-  player = Game.new("Ikraam", "Kubilay")
+  player = Game.new('Ikraam', 'Kubilay')
   describe '#initialize' do
+    it 'Reurns an ' do
+      expect { Game.new('Ikraam', 'Kubilay') }.not_to raise_error(ArgumentError)
+    end
+  end
+  describe '#names' do
     it 'Reurns an array with two inputs and 5 times' do
-      expect(player.names).to eq(["Ikraam", "Kubilay"] * 5)
+      expect(player.names).to eq(%w[Ikraam Kubilay] * 5)
     end
   end
   describe '#board' do
-    it 'returns an array with 9 integers as 0 or 1 or 10' do
+    it 'returns an array with 9 integers of 0' do
+      expect(player.board).to eq(Array.new(9, 0))
+    end
+  end
+  describe '#greeting' do
+    it 'returns the same as player.board passed to show_board_hint' do
+      expect(player.greeting).to eq(show_board_hint(player.board))
+    end
+  end
+end
 
     end
   end
